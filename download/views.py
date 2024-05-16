f7b517dab5d98d881b77e51c493321d80b44678e
Rafiirsyads <rafiirsyad2004@gmail.com>
2024-05-16 21:20:30 +0700

    Downloaded Song Done

--- download/views.py
+++ download/views.py
@@ -1,16 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from utils.query import query
 
 # Create your views here.
 def downloaded_songs(request):
-    # Data lagu statis yang akan ditampilkan di tabel
-    songs = [
-        {'title': 'Song1', 'artist': 'Artist1', 'download_date': '20/02/2024'},
-        {'title': 'Song2', 'artist': 'Artist2', 'download_date': '21/03/2024'},
-    ]
+    akun = request.session.get('akun', None)
+
+    if akun['premium'] == True:
+        print(1)
+    else:
+        print(0)
+
+    email = akun['email']
+
+    query_str = f"""
+    SELECT k.judul AS title, a.nama AS artist
+    FROM marmut.downloaded_song ds
+    JOIN marmut.song s ON ds.id_song = s.id_konten
+    JOIN marmut.artist ar ON s.id_artist = ar.id
+    JOIN marmut.akun a ON ar.email_akun = a.email
+    JOIN marmut.konten k ON s.id_konten = k.id
+    WHERE ds.email_downloader = '{email}';
+    """
+
+    songs = query(query_str)
+    print(songs)
     
     # Render template dengan data lagu yang diberikan
     return render(request, 'downloaded_songs.html', {'songs': songs})
 
 def song_deleted(request, song_title):
-    # Render halaman konfirmasi penghapusan dengan judul lagu
+    # Query untuk menghapus lagu berdasarkan judul
+    query_str = f"""
+    DELETE FROM marmut.downloaded_song
+    USING marmut.konten
+    WHERE marmut.downloaded_song.id_song = marmut.konten.id
+    AND marmut.konten.judul = '{song_title}';
+    """
+
+    # Eksekusi query
+    query(query_str)
+
+    # Render a confirmation page instead of redirecting
     return render(request, 'song_deleted.html', {'song_title': song_title})
