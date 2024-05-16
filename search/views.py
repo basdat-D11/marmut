8ddd390da9382e6494d7ed1fa849db48fed96f8c
Rafiirsyads <rafiirsyad2004@gmail.com>
2024-05-16 18:09:52 +0700

    Search Bar Done

--- search/views.py
+++ search/views.py
@@ -1,27 +1,47 @@
 from django.shortcuts import render
+from utils.query import query
 
-# Create your views here.
-# View untuk menampilkan search bar
+# View for displaying the search bar
 def search_bar(request):
     return render(request, 'search_bar.html')
 
-# View untuk menampilkan hasil pencarian
+# View for displaying search results
 def search_results(request):
-    query = request.GET.get('query', '')
-    context = {'query': query}  # Add 'query' to the context
+    search = request.GET.get('search', '')
+    context = {'search': search}
 
-    # Check if the query is not empty
-    if query:
+    if search:
+        formatted_search = f'%{search}%'
+        search_query = f"""
+        SELECT konten.judul AS Title, 'Song' AS ContentType, akun.nama AS CreatorName
+        FROM song
+        JOIN konten ON song.id_konten = konten.id
+        JOIN artist ON song.id_artist = artist.id
+        JOIN akun ON artist.email_akun = akun.email
+        WHERE konten.judul ILIKE '{formatted_search}'
 
-        if query.lower() == "love":
-            data = [
-                {'type': 'SONG', 'title': 'Love is in the air', 'by': 'Artist1'},
-                {'type': 'SONG', 'title': 'What is love', 'by': 'Artist2'},
-                {'type': 'PODCAST', 'title': 'Love is Blind Pod', 'by': 'Podcaster1'},
-                {'type': 'USER PLAYLIST', 'title': '90s Love Songs', 'by': 'User1'},
-            ]
-            context['results'] = data
-        else:
-            pass
+        UNION
+
+        SELECT konten.judul AS Title, 'Podcast' AS ContentType, akun.nama AS CreatorName
+        FROM podcast
+        JOIN konten ON podcast.id_konten = konten.id
+        JOIN podcaster ON podcast.email_podcaster = podcaster.email
+        JOIN akun ON podcaster.email = akun.email
+        WHERE konten.judul ILIKE '{formatted_search}'
+
+        UNION
+
+        SELECT user_playlist.judul AS Title, 'User Playlist' AS ContentType, akun.nama AS CreatorName
+        FROM user_playlist
+        JOIN akun ON user_playlist.email_pembuat = akun.email
+        WHERE user_playlist.judul ILIKE '{formatted_search}'
+
+        ORDER BY Title ASC;
+        """
+        data = query(search_query)
+        
+        print(data)
+
+        context['results'] = data
 
     return render(request, 'search_result.html', context)
