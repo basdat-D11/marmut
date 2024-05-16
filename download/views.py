5db2a06a584bfd133ed408b629ff462739399c8f
Rafiirsyads <rafiirsyad2004@gmail.com>
2024-05-16 21:26:21 +0700

    Edit akses premium downloaded song

--- download/views.py
+++ download/views.py
@@ -6,27 +6,25 @@ def downloaded_songs(request):
     akun = request.session.get('akun', None)
 
     if akun['premium'] == True:
-        print(1)
+        email = akun['email']
+
+        query_str = f"""
+        SELECT k.judul AS title, a.nama AS artist
+        FROM marmut.downloaded_song ds
+        JOIN marmut.song s ON ds.id_song = s.id_konten
+        JOIN marmut.artist ar ON s.id_artist = ar.id
+        JOIN marmut.akun a ON ar.email_akun = a.email
+        JOIN marmut.konten k ON s.id_konten = k.id
+        WHERE ds.email_downloader = '{email}';
+        """
+
+        songs = query(query_str)
+        
+        # Render template dengan data lagu yang diberikan
+        return render(request, 'downloaded_songs.html', {'songs': songs})
     else:
-        print(0)
-
-    email = akun['email']
-
-    query_str = f"""
-    SELECT k.judul AS title, a.nama AS artist
-    FROM marmut.downloaded_song ds
-    JOIN marmut.song s ON ds.id_song = s.id_konten
-    JOIN marmut.artist ar ON s.id_artist = ar.id
-    JOIN marmut.akun a ON ar.email_akun = a.email
-    JOIN marmut.konten k ON s.id_konten = k.id
-    WHERE ds.email_downloader = '{email}';
-    """
-
-    songs = query(query_str)
-    print(songs)
-    
-    # Render template dengan data lagu yang diberikan
-    return render(request, 'downloaded_songs.html', {'songs': songs})
+        # Render template dengan pesan "Anda tidak dapat menggunakan fitur ini"
+        return render(request, 'downloaded_songs.html', {'premium': False})
 
 def song_deleted(request, song_title):
     # Query untuk menghapus lagu berdasarkan judul
