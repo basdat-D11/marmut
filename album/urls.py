7c90b7ed1912ed302450b65bd8fc5e834d415877
raihanakbr <muhammad.raihan22@ui.ac.id>
2024-05-17 20:38:58 +0700

    func album royalti done

--- album/urls.py
+++ album/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create', album_create, name='album_create'),
     path('<str:id>/add-song', tambah_lagu, name='tambah_lagu'),
     path('<str:id>/list-song', daftar_lagu, name='daftar_lagu'),
+    path('delete/<str:id>', delete_album, name='delete_album'),
+    path('delete-song/<str:id>', delete_lagu, name='delete_album')
 ]
