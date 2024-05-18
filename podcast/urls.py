dab12a5f0be8a0362980c82e0dbf59007a0a318b
mmaraaap <ammara.pranahiza@ui.ac.id>
2024-05-18 23:32:25 +0700

    coba delete

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import kelola_podcast, create_podcast, list_podcast, create_episode, daftar_episode, delete_podcast, delete_episode, play_podcast
+from .views import kelola_podcast, create_podcast, list_podcast, create_episode, daftar_episode, play_podcast, delete_podcast, delete_episode
 
 app_name = 'podcast'
 
@@ -9,7 +9,9 @@ urlpatterns = [
     path('list_podcast/', list_podcast, name='list_podcast'),
     path('create_episode/', create_episode, name='create_episode'),
     path('daftar_episode/', daftar_episode, name='daftar_episode'),
-    path('delete_podcast/', delete_podcast, name='delete_podcast'),
-    path('delete_episode/', delete_episode, name='delete_episode'),
     path('play_podcast/', play_podcast, name='play_podcast'),
+    path('delete_episode/', delete_episode, name='delete_episode'),
+    path('delete_podcast/', delete_podcast, name='delete_podcast'),
+    
+
 ]
